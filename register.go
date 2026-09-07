5379b9e1d1b819bcdfcb5327dd5c198fe4cbbe4f
junya koyama <arukiidou@yahoo.co.jp>
2026-09-08 00:59:37 +0900

    Add pkg comments

--- register.go
+++ register.go
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: 2026 arukiidou <arukiidou@yahoo.co.jp>
 // SPDX-License-Identifier: Apache-2.0
 
+// Package xk6dukpt is a k6 extension for DUKPT (Derived Unique Key Per Transaction) cryptography operations.
 package xk6dukpt
 
 import "go.k6.io/k6/v2/js/modules"
