49404c3f1e7b44fbc31be9e1e00d0d4207099ca9
junya koyama <arukiidou@yahoo.co.jp>
2026-09-08 00:43:10 +0900

    Add EncryptPin smoke tests

--- test/dukpt.test.js
+++ test/dukpt.test.js
@@ -1,6 +1,6 @@
 import { check } from "k6";
 import encoding from 'k6/encoding';
-import { derivationOfInitialKey, deriveCurrentTransactionKey } from "k6/x/dukpt";
+import { derivationOfInitialKey, deriveCurrentTransactionKey, encryptPin } from "k6/x/dukpt";
 
 export const options = {
   thresholds: {
@@ -11,21 +11,27 @@ export const options = {
 export default async function () {
   const ikExpected = "asKS+qExW02FirOj19WTOg==";//"0123456789ABCDEFFEDCBA9876543210";
   const ckExpected = "BCZmtJGEz6No3pYo0Dl7yQ=="; //"042666B49184CFA368DE9628D0397BF9";
-  
+  const pinEncExpected = "G5wYReuZOno="; //"1B9C1845EB993A7A";
+
   // TODO: wait for merge Uint8Array.fromBase64() to sobek
   const bdk = encoding.b64decode("ASNFZ4mrze/+3LqYdlQyEA=="); //"0123456789ABCDEFFEDCBA9876543210";
   const ksn = encoding.b64decode("//+YdlQyEOAAAQ=="); //"FFFF9876543210E00001";
 
   const ik = derivationOfInitialKey(bdk, ksn);
-  console.debug("Actual: " + ik);
-  console.debug("Expected: " + ikExpected);
-
   const ck = deriveCurrentTransactionKey(ik, ksn);
-  console.debug("Actual: " + encoding.b64encode(ck));
-  console.debug("Expected: " + ckExpected);
 
   check(null, {
     'derivationOfInitialKey(bdk, ksn)': () => encoding.b64encode(ik) === ikExpected,
     'deriveCurrentTransactionKey(ik, ksn)': () => encoding.b64encode(ck) === ckExpected,
+  });
+
+  const pin = "1234";
+  const pan = "4012345678909";
+  const format = "ISO-0";
+
+  const pinEnc = encryptPin(ck, pin, pan, format);
+
+  check(null, {
+    'encryptPin(ck, pin, pan, format)': () => encoding.b64encode(pinEnc) === pinEncExpected,
   });
 }
--- test/dukptBase64.test.js
+++ test/dukptBase64.test.js
@@ -1,5 +1,5 @@
 import { check } from "k6";
-import { derivationOfInitialKeyAsBase64, deriveCurrentTransactionKeyAsBase64 } from "k6/x/dukpt";
+import { derivationOfInitialKeyAsBase64, deriveCurrentTransactionKeyAsBase64, encryptPinAsBase64 } from "k6/x/dukpt";
 
 export const options = {
   thresholds: {
@@ -10,20 +10,26 @@ export const options = {
 export default function () {
   const ikExpected = "asKS+qExW02FirOj19WTOg==";//"0123456789ABCDEFFEDCBA9876543210";
   const ckExpected = "BCZmtJGEz6No3pYo0Dl7yQ=="; //"042666B49184CFA368DE9628D0397BF9";
+  const pinEncExpected = "G5wYReuZOno="; //"1B9C1845EB993A7A";
 
   const bdk = "ASNFZ4mrze/+3LqYdlQyEA=="; //"0123456789ABCDEFFEDCBA9876543210";
   const ksn = "//+YdlQyEOAAAQ=="; //"FFFF9876543210E00001";
 
   const ik = derivationOfInitialKeyAsBase64(bdk, ksn)
-  console.debug("Actual: " + ik);
-  console.debug("Expected: " + ikExpected);
-
   const ck = deriveCurrentTransactionKeyAsBase64(ik, ksn)
-  console.debug("Actual: " + ck);
-  console.debug("Expected: " + ckExpected);
 
   check(null, {
     'derivationOfInitialKeyAsBase64(bdk, ksn)': () => ik === ikExpected,
     'deriveCurrentTransactionKeyAsBase64(ik, ksn)': () => ck === ckExpected,
+  });
+
+  const pin = "1234";
+  const pan = "4012345678909";
+  const format = "ISO-0";
+  
+  const pinEnc = encryptPinAsBase64(ck, pin, pan, format)
+
+  check(null, {
+    'encryptPinAsBase64(ck, pin, pan, format)': () => pinEnc === pinEncExpected,
   });
 }
